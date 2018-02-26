8c8e29467ff9aa9e2b3be0f28d94b678bad7026e
gaurang1589 <Gaurang@gaurangs-mbp.mshome.net>
2018-02-26 10:32:10 +0100

    Added TestExplosivesJunit4Public class

---
+++ src/TestExplosivesJUnit4Public.java
@@ -0,0 +1,83 @@
+import static org.junit.Assert.*;
+
+import org.jmlspecs.utils.JmlAssertionError;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestExplosivesJUnit4Public {
+	
+	static int nb_inconclusive = 0;
+    static int nb_fail = 0;
+
+    Explosives e;
+
+    public static void main(String args[]) {
+    	String testClass = "TestExplosivesJUnit4";
+     	org.junit.runner.JUnitCore.main(testClass);
+     }
+
+
+    private void handleJMLAssertionError(JmlAssertionError e) {
+    	if (e.getClass().equals(JmlAssertionError.PreconditionEntry.class)) {
+    	    System.out.println("\n INCONCLUSIVE "+(new Exception().getStackTrace()[1].getMethodName())+ "\n\t "+ e.getMessage());
+            nb_inconclusive++;}
+    else{
+	    // test failure	
+	    nb_fail++;
+	    fail("\n\t" + e.getMessage());	
+		}  
+    }
+	
+    
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		nb_inconclusive = 0;
+		nb_fail = 0;
+		org.jmlspecs.utils.Utils.useExceptions = true;
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	     System.out.println("\n inconclusive tests: "+nb_inconclusive+" -- failures : "+nb_fail );
+	}
+	
+	@Test
+	public void testSequence_1() {
+		try{
+			e=new Explosives();
+			e.nb_inc = 50;
+			e.skip();
+
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}
+	}
+	
+	@Test
+	public void testSequence_1_1() {
+		try{
+			e=new Explosives();
+			e.nb_assign = 30;
+			e.skip();
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}
+	}
+	
+	@Test
+	public void testSequence_6() {
+		try{
+			e=new Explosives();
+			e.add_incomp("Prod_Nitro","Prod_Glycerine");
+			e.nb_inc--;
+			e.incomp[e.nb_inc][0] = null;
+			e.incomp[e.nb_inc][1] = null;
+			e.skip();
+
+		} 	catch(JmlAssertionError e){
+				handleJMLAssertionError(e);		
+		}  
+	}
+
+}
